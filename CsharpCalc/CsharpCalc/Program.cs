c0aaa57ba60bbfe77f8301c9a2f0a822e457a560
Miamba26 <asgharibrahim26@gmail.com>
2022-05-30 18:00:29 +0100

    addition

---
+++ CsharpCalc/CsharpCalc/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CsharpCalc
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = Addition(a, b);
+
+            Console.WriteLine("Sum is: " + sum);
+        }
+
+        private static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
