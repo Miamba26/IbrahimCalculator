1d19d4f88f2aa269314739cae3c4e132a8dc3715
Miamba26 <asgharibrahim26@gmail.com>
2022-05-31 15:18:25 +0100

    put all my apps in here

---
+++ CsharpApps/CsharpApps/Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpApps
+{
+    internal class Calculator
+    {
+        public void RunApp()
+        {
+            Console.Write("Running Calulator...");
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Enter first number: ");
+                string? num1Str = Console.ReadLine();
+
+                Console.Write("Enter operation (A-Add, S-Subtract, M-Multiply, D-Divide): ");
+                string? operation = Console.ReadLine();
+
+                Console.Write("Enter second number: ");
+                string? num2Str = Console.ReadLine();
+
+                int? result = Calculate(num1Str, operation, num2Str);
+
+                if (result != null)
+                {
+                    Console.WriteLine("Result is: " + result);
+                }
+                else
+                {
+                    Console.WriteLine($"Could not calculate {num1Str} {operation} {num2Str}");
+                }
+
+                Console.WriteLine();
+            } while (true);
+        }
+
+        private int? Calculate(string? num1Str, string? operation, string? num2Str)
+        {
+            int? result = null;
+
+            // check inputs are valid
+
+            int? num1 = null;
+            if (int.TryParse(num1Str, out int number1))
+            {
+                num1 = number1;
+            }
+
+            int? num2 = null;
+            if (int.TryParse(num2Str, out int number2))
+            {
+                num2 = number2;
+            }
+
+            switch (operation)
+            {
+                case "A":
+                case "a":
+                    result = num1 + num2;
+                    break;
+
+                case "B":
+                case "b":
+                    result = num1 - num2;
+                    break;
+
+                case "C":
+                case "c":
+                    result = num1 * num2;
+                    break;
+
+                case "D":
+                case "d":
+                    result = num1 / num2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
--- CsharpApps/CsharpApps/Program.cs
+++ CsharpApps/CsharpApps/Program.cs
@@ -4,74 +4,11 @@
     {
         public static void Main()
         {
-            do
-            {
-                Console.Write("Enter first number: ");
-                string? num1Str = Console.ReadLine();
+            Calculator calculator = new();
+            calculator.RunApp();
 
-                Console.Write("Enter operation (A-Add, S-Subtract, M-Multiply, D-Divide): ");
-                string? operation = Console.ReadLine();
-
-                Console.Write("Enter second number: ");
-                string? num2Str = Console.ReadLine();
-
-                int? result = Calculate(num1Str, operation, num2Str);
-
-                if (result != null)
-                {
-                    Console.WriteLine("Result is: " + result);
-                }
-                else
-                {
-                    Console.WriteLine($"Could not calculate {num1Str} {operation} {num2Str}");
-                }
-
-                Console.WriteLine();
-            } while (true);
-        }
-
-        private static int? Calculate(string? num1Str, string? operation, string? num2Str)
-        {
-            int? result = null;
-
-            // check inputs are valid
-
-            int? num1 = null;
-            if (int.TryParse(num1Str, out int number1))
-            {
-                num1 = number1;
-            }
-
-            int? num2 = null;
-            if (int.TryParse(num2Str, out int number2))
-            {
-                num2 = number2;
-            }
-
-            switch (operation)
-            {
-                case "A":
-                case "a":
-                    result = num1 + num2;
-                    break;
-
-                case "B":
-                case "b":
-                    result = num1 - num2;
-                    break;
-
-                case "C":
-                case "c":
-                    result = num1 * num2;
-                    break;
-
-                case "D":
-                case "d":
-                    result = num1 / num2;
-                    break;
-            }
-
-            return result;
+            RockPaperScissors rps = new();
+            rps.RunApp();
         }
     }
 }
